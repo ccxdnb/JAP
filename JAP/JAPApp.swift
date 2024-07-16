559f5c108e2fb345c0d18cc0e3a5a10cf36c46dd
jwilson <jwilsonrae@gmail.com>
2024-07-16 19:25:49 +0200

    initial commit, empty project

---
+++ JAP/JAPApp.swift
@@ -0,0 +1,17 @@
+//
+//  JAPApp.swift
+//  JAP
+//
+//  Created by Joaquin Wilson on 16-07-24.
+//
+
+import SwiftUI
+
+@main
+struct JAPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
